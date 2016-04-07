59f3335e12ed79a233df359f131d57c4500f3a63
shihabmi7 <shihab.mi7@gmail.com>
2016-04-07 11:50:29 +0600

    //  update : with floating bar

--- app/src/main/java/roy/tablayoutwithviewpager/MainActivity.java
+++ app/src/main/java/roy/tablayoutwithviewpager/MainActivity.java
@@ -1,21 +1,39 @@
 package roy.tablayoutwithviewpager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     ViewPager pager;
     TabLayout tabLayout;
 
+    FloatingActionButton fab;
     // test \
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        fab =(FloatingActionButton)findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Click action
+                Intent intent = new Intent(MainActivity.this, FishItemAddActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        setSupportActionBar(toolbar);
 
         pager = (ViewPager) findViewById(R.id.view_pager);
         tabLayout = (TabLayout) findViewById(R.id.tab_layout);
@@ -32,4 +50,6 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.setTabsFromPagerAdapter(adapter);
 
     }
+
+
 }
